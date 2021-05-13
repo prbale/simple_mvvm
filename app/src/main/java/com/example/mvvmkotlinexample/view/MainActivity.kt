f813ababa53f01e65c978aa469b9e3fa646b7345
Prashant <bale.prashant1984@gmail.com>
2021-05-13 18:55:41 +0530

    Changes

--- app/src/main/java/com/example/mvvmkotlinexample/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmkotlinexample/view/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mvvmkotlinexample.view
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
